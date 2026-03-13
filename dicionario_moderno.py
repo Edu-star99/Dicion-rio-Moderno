17ef71138335d447969e6eb630d909b2250da142
Edu-star99 <dudumf1302@gmail.com>
2026-03-13 20:54:53 -0300

    Update dicionario_moderno.py

--- dicionario_moderno.py
+++ dicionario_moderno.py
@@ -1,1 +1,49 @@
+meme_dict = {
+            "CRINGE": "Significado: Algo vergonhoso ou constrangedor",
+            "STALKEAR": "Significado: Investigar a vida de alguém online",
+            "VDD": "Significado: Abreviação da palavra: verdade",
+            "BISCOITAR": "Significado: Postar algo apenas para chamar atenção",
+            "HATER": "Significado: Pessoa que está constantemente criticando os outros",
+            "VLW": "Significado: Abreviação da palavra: valeu",
+            "DND": "Significado: Abreviação da palavra: denada",
+            "PODE PÁ": "Significado: Pode significar algo como: é mesmo",
+            "LOOTEAR": "Significado: Saquear algo para pegar coisas melhores (armas melhores em jogos de tiro por exemplo)",
+            "PAIA": "Significado: Algo que parecia uma coisa mas foi outra"
+            }
 
+word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Essa palavra infelizmente não existe no nosso dicionario, desculpe :(")
+
+word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Essa palavra infelizmente não existe no nosso dicionario, desculpe :(")
+
+word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Essa palavra infelizmente não existe no nosso dicionario, desculpe :(")
+
+word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Essa palavra infelizmente não existe no nosso dicionario, desculpe :(")
+
+word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Essa palavra infelizmente não existe no nosso dicionario, desculpe :(")
+
+# Eu sei que era só colocar um loop, mas não lembro como faz ele :(
